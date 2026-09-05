df6fb90e5919d8305da3b0242e6c9770e1867b68
gunar1685 <gunar.info@gmail.com>
2026-09-05 17:12:29 +0200

    V1

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "zeit-detektiv-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Beim Installieren: alle Dateien in den Cache legen
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Beim Aktivieren: alte Caches aufräumen
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Bei Anfragen: zuerst aus dem Cache, sonst aus dem Netzwerk laden
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
+    })
+  );
+});
